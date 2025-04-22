71a6b3cda6ea824c025b6b8a98f4dc7f26825f6c
Cuong991 <125679778+Cuong991@users.noreply.github.com>
2025-04-22 14:43:58 +0700

    Update Nutbam3.py

--- Nutbam3.py
+++ Nutbam3.py
@@ -1,15 +1,11 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
-async def handle_button3(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def handle_Nut_bam_3(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
 
     chat_id = query.message.chat_id
+    message = "Bạn vừa bấm nút số 3 thành công!"
 
-    # Nội dung bạn muốn gửi khi bấm nút 3
-    await context.bot.send_message(
-        chat_id=chat_id,
-        text="Bạn vừa bấm nút 3!",
-        parse_mode="HTML"
-    )
+    await context.bot.send_message(chat_id=chat_id, text=message)
